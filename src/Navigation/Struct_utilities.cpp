754171646f392118cc539c50a97a429ca3f76ba9
MrIvanushka <74343782+MrIvanushka@users.noreply.github.com>
2024-03-17 07:08:16 +0300

    [Navigation] + Добавил поиск пути и генерацию карты навигаций

---
+++ src/Navigation/Struct_utilities.cpp
@@ -0,0 +1,26 @@
+#include "Struct_utilities.h"
+
+poly_intersection::poly_intersection() {};
+
+poly_intersection::poly_intersection(const poly_intersection& other) : line(other.line) {};
+
+poly_intersection& poly_intersection::operator=(const poly_intersection& other){
+    this->line = other.line;
+    return *this;
+}
+
+Node::Node(){}
+
+Node::Node(const TPPLPoly_it& p, double score) : poly(p), f_score(score){}
+
+Node::Node(const Node& other) : poly(other.poly), f_score(other.f_score) {}
+
+Node& Node::operator=(const Node& other){
+    poly = other.poly;
+    f_score = other.f_score;
+    return *this;
+}
+
+bool Node::operator<(const Node& other) const{
+    return (other.f_score < f_score);
+}
